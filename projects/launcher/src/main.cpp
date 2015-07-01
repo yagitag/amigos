695167ce8487ba26aeccdf32f0838c5c4e8647f5
kpd0 <krylovpd@gmail.com>
2015-07-02 02:23:28 +0300

    path_to_bigarams getter added

--- projects/launcher/src/main.cpp
+++ projects/launcher/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "../../../include/launcher/launcher.hpp"
 #include "../../../include/searcher/searcher.hpp"
 
@@ -6,11 +7,16 @@ using namespace std;
 
 int main(int argc, char *argv[]) 
 {
+    if( argc != 4 )
+    {
+        std::cerr << "USAGE: " << string(argv[0]) << " path_to_config path_to_stopwords query" << std::endl;
+        return 1;
+    }
     Launcher launcher;
-    launcher.configure(string(argv[1]), "");
+    launcher.configure(string(argv[1]), string(argv[2]));
 
     vector<Document> docs;
-    string query = string(argv[2]);
+    string query = string(argv[3]);
     launcher.launch_searcher( query, docs );
 
     std::cout << docs.size() << std::endl;   
